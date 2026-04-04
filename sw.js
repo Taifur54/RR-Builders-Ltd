5be787a0bc54424ef7e261295a08e5511d95c136
Taifur54 <rragrofarmsbd@gmail.com>
2026-04-05 02:01:48 +0600

    Bump cache version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'rr-builders-v1';
+const CACHE = 'rr-builders-v2';
 const ASSETS = ['/RR-Builders-Ltd/tablet.html', '/RR-Builders-Ltd/manifest.json'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(() => {})); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
